6c5f2b80842329e4368e276926214c59dc2e4d9f
diarmate <andokeita0@gmail.com>
2018-11-13 21:40:55 +0900

    20181113-2140

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LANG=en_US.utf8
 NowDate=$(date +%Y%m%d)-$(date +%H%M) 
-cd /volume1/web/git/diarmate.github.io
+cd /volume1/web/makerss/git/diarmate.github.io
 git add *
 git commit -m $NowDate
 git push
